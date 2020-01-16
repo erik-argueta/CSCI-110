17b7486a1506f940e6f8438efe1c4dc25a94d75e
erik-argueta <59490229+erik-argueta@users.noreply.github.com>
2020-01-16 13:48:45 -0800

    Lab003b

---
+++ Labs/Lab003b/Lab003b/lab003b.cpp
@@ -0,0 +1,35 @@
+/*
+ Write a function void sort2(int& a, int& b) that swaps the values of a and b if a is greater than b and otherwise leaves a and b unchanged. For example,
+ int u = 2;
+ int v = 3;
+ int w = 4;
+ int x = 1;
+ sort2(u, v); // u is still 2, v is still 3
+ sort2(w, x); // w is now 1, x is now 4
+ */
+
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int u = 2;
+int v = 3;
+int w = 4;
+int x = 1;
+
+void sort2(int& a, int& b)
+{
+	
+}
+
+
+
+int main()
+{
+	cout << "u = 2" << endl << "v = 3" << endl;
+	cout << "w = 4" << endl << "x = 1" << endl;
+	
+	return 0;
+}
