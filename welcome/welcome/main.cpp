7b8637ae92f6a5554dac8ac4a957fd79d7634479
erik-argueta <59490229+erik-argueta@users.noreply.github.com>
2020-01-16 01:32:05 -0800

    initialize

---
+++ welcome/welcome/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  welcome
+//
+//  Created by Erik Argueta on 1/16/20.
+//  Copyright © 2020 Erik Argueta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
