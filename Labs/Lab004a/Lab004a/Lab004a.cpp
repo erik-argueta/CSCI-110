3cbba01ff01c23e982d66645d48a950f5e3bfc1e
erik-argueta <59490229+erik-argueta@users.noreply.github.com>
2020-01-29 19:05:36 -0800

    Uploading In-Class assignments

--- Labs/Lab004a/Lab004a/Lab004a.cpp
+++ Labs/Lab004a/Lab004a/Lab004a.cpp
@@ -8,84 +8,117 @@ bool equals(int a[], int a_size, int b[], int b_size)
 that checks whether two arrays have the same elements in the same order
 */
 
+/*
+Need to set a route:
+1. User must input Set A, Set B.
+ 
+refer to page 261
+*/
 
-
-//#include "stdafx.h"
+//include "stdafx.h"
 #include <iostream>
 
 using namespace std;
 
-const int CAPACITY_A = 4;
-const int CAPACITY_B = 4;
-int values_a = 0;
-int values_b = 0;
 
-bool equal(int input_a[CAPACITY_A], int curent_size_a, int b[CAPACITY_B], int current_size_b)
+
+/* bool equal(int input_a[CAPACITY_A], int curent_size_a, int b[CAPACITY_B], int current_size_b)
 {
-	
-	return true;
+int pos = 0;
+int counter = 0;
+bool check1 = false;
+
+while (pos != CAPACITY_A && pos != CAPACITY_B && check1)
+{
+if (array_a[pos] == array_b[pos])
+{
+counter++;
+pos++;
+}
+else
+{
+pos++;
+}
 }
 
+
+return true;
+}
+*/
 
 
 
 int main()
 {
-	const int CAPACITY_A = 4;
-	const int CAPACITY_B = 4;
-	int values_a[CAPACITY_A];
-	int values_b[CAPACITY_B];
-	int current_size_a = 0;
-	int current_size_b = 0;
+	const int CAPACITY_A = 5;
+	const int CAPACITY_B = 5;
 	int input_a;
 	int input_b;
+	int current_size_a = 0;
+	int current_size_b = 0;
+	int array_a[CAPACITY_A];
+	int array_b[CAPACITY_B];
+	bool set_a = false;
+	bool set_b = false;
 
-	// Arrays
+	int array_z[5] = { 1, 2, 3, 4, 5 };
+
+	// This is an example of an Array
 	{
-		// Set1
+		for (int z = 0; z < 5; z++)
 		{
-			cout << "Please enter a series of 5 numbers with spaces inbetween for Set A" << endl << "(i.e. 1 2 3 4 5 ...)" << endl;
-			while (cin >> input_a && current_size_a < CAPACITY_A)
-			{
-				if (current_size_a < CAPACITY_A)
-				{
-					values_a[current_size_a] = input_a;
-					current_size_a++;
-				}
-			}
-			cout << "Set A received" << endl << endl;
+			cout << array_z[z] << endl;
 		}
+	}
 
-		//Set 2
+	cout << endl << endl;
+	// Array A
+	{
+		cout << "Please enter five values for Set A (i.e. 1 2 3 4 5)" << endl;
+		cout << "Set A: ";
+
+		while (current_size_a != CAPACITY_A)
 		{
-			cout << "Please enter a series of 5 numbers with spaces inbetween for Set B" << endl << "(i.e. 1 2 3 4 5 ...)" << endl;
-			while (cin >> input_b && current_size_b < CAPACITY_B)
+			cin >> input_a;
+			if (current_size_a != CAPACITY_A)
 			{
-				if (current_size_b < CAPACITY_B)
-				{
-					values_b[current_size_b] = input_b;
-					current_size_b++;
-				}
+				array_a[current_size_a] = input_a;
+				current_size_a++;
 			}
-			cout << "Set B received" << endl << endl;
 		}
+		
+		for (int n = 0; n != current_size_a; n++)
+		{
+			cout << array_a[n] << endl;
+		}
+	}
+		
+	// Array B
+	{
 		cout << endl;
-	}
-	
-	//equal(input_a[CAPACITY_A], input_b[CAPACITY_B]);
-	
-	
+		cout << "Please enter five numbers for Set B" << endl;
+		cout << "Set B: ";
 
-	/* if ()
-	{
-		cout << "The two sets are exactly the same." << endl;
+		while (current_size_b != CAPACITY_B)
+		{
+			cin >> input_b;
+			if (current_size_b != CAPACITY_B)
+			{
+				array_b[current_size_b] = input_b;
+				current_size_b++;
+			}
+		}
+		
+		for (int n = 0; n != current_size_b; n++)
+		{
+			cout << array_b[n] << endl;
+		}
 	}
-	else
-	{
-		cout << "The two sets are different." << endl;
-	} */
+
+
+	
 
 	//system("pause");
-    return 0;
+	return 0;
 }
 
