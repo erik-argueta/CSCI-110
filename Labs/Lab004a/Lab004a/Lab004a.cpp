03c678f90340d075668c377be8d2eb1e5475d6db
erik-argueta <59490229+erik-argueta@users.noreply.github.com>
2020-02-02 01:05:53 -0800

    Completed Lab004a -- Trial 1

--- Labs/Lab004a/Lab004a/Lab004a.cpp
+++ Labs/Lab004a/Lab004a/Lab004a.cpp
@@ -9,27 +9,39 @@ that checks whether two arrays have the same elements in the same order
 */
 
 
-//include "stdafx.h"
+//#include "stdafx.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-const int CAPACITY_A = 5;
-const int CAPACITY_B = 5;
+const int CAPACITY_A = 10;
+const int CAPACITY_B = 10;
+const int CAPACITY_C = 10;
+const int CAPACITY_D = 10;
 int input_a;
 int input_b;
+int input_c;
+int input_d;
 int counter = 0;
+int counter2 = 0;
 int current_size_a = 0;
 int current_size_b = 0;
+int current_size_c = 0;
+int current_size_d = 0;
 int array_a[CAPACITY_A];
 int array_b[CAPACITY_B];
+int array_c[CAPACITY_C];
+int array_d[CAPACITY_D];
+
+
 
 int a(int input_a)
 {
-	while (current_size_a != CAPACITY_A)
+	while (current_size_a < CAPACITY_A)
 	{
 		cin >> input_a;
-		if (current_size_a != CAPACITY_A)
+		if (current_size_a < CAPACITY_A)
 		{
 			array_a[current_size_a] = input_a;
 			current_size_a++;
@@ -38,12 +50,13 @@ int a(int input_a)
 	return array_a[current_size_a];
 }
 
+
 int b(int input_b)
 {
-	while (current_size_b != CAPACITY_B)
+	while (current_size_b < CAPACITY_B)
 	{
 		cin >> input_b;
-		if (current_size_b != CAPACITY_B)
+		if (current_size_b < CAPACITY_B)
 		{
 			array_b[current_size_b] = input_b;
 			current_size_b++;
@@ -52,58 +65,135 @@ int b(int input_b)
 	return array_b[current_size_b];
 }
 
-bool equals(int array_a[current_size_a], int array_b[current_size_b])
+int c(int input_c)
 {
-	for (int pos = 0; pos != 5; pos++)
+	while (current_size_c < CAPACITY_C)
+	{
+		cin >> input_c;
+		if (current_size_c < CAPACITY_C)
+		{
+			array_c[current_size_c] = input_c;
+			current_size_c++;
+		}
+	}
+	return array_c[current_size_c];
+}
+
+int d(int input_d)
+{
+	while (current_size_d < CAPACITY_D)
+	{
+		cin >> input_d;
+		if (current_size_d < CAPACITY_D)
+		{
+			array_d[current_size_d] = input_d;
+			current_size_d++;
+		}
+	}
+	return array_d[current_size_d];
+}
+
+bool equals(int a, int b)
+{
+	if (counter >= 10)
+	{
+		return false;
+	}
+
+	else
+	{
+		return true;
+	}
+}
+
+bool equals2(int c, int d)
+{
+	if (counter2 >= 10)
+	{
+		return false;
+	}
+
+	else
+	{
+		return true;
+	}
+}
+
+int main()
+{
+	cout << "Please enter two sets of numbers in the SAME sequence." << endl;
+	cout << "There must be ten values for each set." << endl << endl;
+
+	cout << "Set A: ";
+	a(input_a);
+
+	//cout << endl;
+
+	cout << "Set B: ";
+	b(input_b);
+
+	cout << endl;
+
+	for (int pos = 0; pos != 10; pos++)
 	{
 		if (array_a[pos] == array_b[pos])
 		{
 			counter++;
 		}
 	}
-	
-	if (counter < 5)
+
+	equals(a(input_a), b(input_b));
+
+
+	if ((equals(a(input_a), b(input_b))))
 	{
-		return false;
+		cout << "The sets are different." << endl;
 	}
-	
-	if (counter > 5)
+
+	if (!equals(a(input_a), b(input_b)))
 	{
-		return true;
+		cout << "The sets are exactly the same." << endl;
 	}
-}
 
-
-int main()
-{
-	int input_a;
-	int input_b;
-
-	/* This is an example of an Array
-	{
-		for (int z = 0; z < 5; z++)
-		{
-			cout << array_z[z] << " || ";
-		}
-	} */
-
-	cout << endl << endl;
+	cout << endl << "--------------------------------------" << endl << endl;
 	
+	
+	cout << "Please enter two sets of numbers in a DIFFERENT sequence." << endl;
+	cout << "There must be ten values for each set." << endl << endl;
 
-	cout << "Please enter five values for Set A (i.e. 1 2 3 4 5)" << endl;
 	cout << "Set A: ";
-	a(input_a);
-	
+	c(input_c);
+
+	//cout << endl;
+
+	cout << "Set B: ";
+	d(input_d);
+
+	cout << endl;
+
+	for (int pos = 0; pos != 10; pos++)
+	{
+		if (array_c[pos] == array_d[pos])
+		{
+			counter2++;
+		}
+	}
+
+	equals2(c(input_c), d(input_d));
+
+
+	if ((equals2(c(input_c), d(input_d))))
+	{
+		cout << "The sets are different." << endl;
+	}
+
+	if (!equals2(c(input_c), d(input_d)))
+	{
+		cout << "The sets are exactly the same." << endl;
+	}
+
 	cout << endl << endl;
 	
-	cout << "Please enter five values for Set B (i.e. 1 2 3 4 5)" << endl;
-	cout << "Set B: ";
-	b(input_b);
-	
-	cout << endl;
-	
-	
-
 
 	//system("pause");
 	return 0;
