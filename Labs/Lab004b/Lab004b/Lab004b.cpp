28c459b3773bcb8738c8d5365627bb675444cc3e
erik-argueta <59490229+erik-argueta@users.noreply.github.com>
2020-02-04 20:35:55 -0800

    Updated Lab004b

--- Labs/Lab004b/Lab004b/Lab004b.cpp
+++ Labs/Lab004b/Lab004b/Lab004b.cpp
@@ -1,40 +1,27 @@
-/*
- 
- Write a function that removes duplicates from an array. For example, if remove_
- duplicates is called with an array containing
-	1 4 9 16 9 7 4 9 11
- then the array is changed to
-	1 4 9 16 7 11
- Your function should have a reference parameter for the array size that is updated when removing the duplicates.
- 
- */
-
-//#include "stdafx.h"
 #include <iostream>
-
 
 using namespace std;
 
-void removeNumber(int array1[], int index, int &size)
+void removeNumber(int numbers[], int index, int& size)
 {
-	for(int i = 0; i < size; i++)
+	for(int i = index; i < size - 1; i++)
 	{
-		array1[i] = array1[i + 1];
+		numbers[i] = numbers[i + 1];
 	}
 	size--;
 }
-void removeDuplicates(int array1[], int& size)
+
+void removeDuplicate(int numbers[], int& size)
 {
-	int i, j;
 	int number;
-	for (i = 0; i < size; i++)
+	for(int i = 0; i < size; i++)
 	{
-		number = array1[i];
-		for (j = i + 1; j < size; j++)
+		number = numbers[i];
+		for(int j = i + 1; j < size; j++)
 		{
-			if (number == array1[j])
+			if(number == numbers[j])
 			{
-				removeNumber(array1, j, size);
+				removeNumber(numbers, j, size);
 				j--;
 			}
 		}
@@ -43,30 +30,28 @@ void removeDuplicates(int array1[], int& size)
 
 int main()
 {
-	int capacity = 9;
-	int array_a[] = {1, 4, 9, 16, 9, 7, 4, 9, 11};
+	int i;
+	int array_1[] = { 1,4,9,16,9,7,4,9,4,11 };
+	int size = 10;
 	
-	cout << "Set 1 (with Duplicates) ";
-	for (int i = 0; i < capacity; i++)
+	cout << "Set (with Duplicates) ";
+	for (int i = 0; i < size; i++)
 	{
-		cout << array_a[i] << " ";
-	}
-	
-	removeDuplicates(array_a, capacity);
-	
-	cout << endl << endl;
-	
-	cout << "Set 1 (without Duplicates) ";
-	for (int i = 0; i < capacity; i++)
-	{
-		cout << array_a[i] << " ";
+		cout << array_1[i] << " ";
 	}
 	
 	cout << endl << endl;
 	
+	removeDuplicate(array_1, size);
 	
-	
+	cout << "Set (without Duplicates) ";
 
-	// system("pause");
-	return 0;
+	for(i = 0; i < size; i++)
+	{
+		cout << array_1[i] << " ";
+	}
+	
+	cout << endl << endl;
+	
+    return 0;
 }
