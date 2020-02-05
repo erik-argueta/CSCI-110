08f4b41d275e03525ba140f9bcceb2ac3bc353bc
erik-argueta <59490229+erik-argueta@users.noreply.github.com>
2020-02-05 12:29:01 -0800

    Updated User Input -- Lab004b

--- Labs/Lab004b/Lab004b/Lab004b.cpp
+++ Labs/Lab004b/Lab004b/Lab004b.cpp
@@ -1,34 +1,39 @@
 /*
- 
- 
- 
- */
 
+Write a function that removes duplicates from an array. For example, if remove_
+duplicates is called with an array containing
+1 4 9 16 9 7 4 9 11
+then the array is changed to
+1 4 9 16 7 11
+Your function should have a reference parameter for the array size that is updated when removing the duplicates.
+
+*/
+
+//#include "stdafx.h"
 #include <iostream>
 
 using namespace std;
 
-void removeNum(int array1[], int index, int& size)
+void removeNumber(int numbers[], int index, int& size)
 {
 	for (int i = index; i < size - 1; i++)
 	{
-		array1[i] = array1[i + 1];
+		numbers[i] = numbers[i + 1];
 	}
 	size--;
 }
 
-
-
-void removeDupl(int array1[], int& size)
+void removeDuplicate(int numbers[], int& size)
 {
+	int number;
 	for (int i = 0; i < size; i++)
 	{
-		int number = array1[i];
+		number = numbers[i];
 		for (int j = i + 1; j < size; j++)
 		{
-			if (number == array1[j])
+			if (number == numbers[j])
 			{
-				removeNum(array1, j, size);
+				removeNumber(numbers, j, size);
 				j--;
 			}
 		}
@@ -37,43 +42,45 @@ void removeDupl(int array1[], int& size)
 
 int main()
 {
-	int CAPACITY = 9;
+	const int CAPACITY = 9;
 	int current_size = 0;
-	int array_a [CAPACITY];
+	int array_1[CAPACITY];
 	int input;
-	
-	cout << "Please enter a series of numbers with duplicate values: ";
+
+
+	cout << "Please insert a sequence of values with duplicate values: ";
 	while (current_size < CAPACITY)
 	{
 		cin >> input;
 		if (current_size < CAPACITY)
 		{
-			array_a[current_size] = input;
+			array_1[current_size] = input;
 			current_size++;
 		}
 	}
-	
+
 	cout << endl;
-	
-	cout << "This is the Array you have entered: ";
+
+	cout << "Array (with Duplicates) ";
 	for (int i = 0; i < current_size; i++)
 	{
-		cout << array_a[i] << " ";
+		cout << array_1[i] << " ";
 	}
-	
+
 	cout << endl << endl;
-	
-	
-	removeDupl(array_a, current_size);
-	
-	cout << "This is the Array you have entered with the Duplicates removed: ";
+
+	removeDuplicate(array_1, current_size);
+
+	cout << "Array (without Duplicates) ";
+
 	for (int i = 0; i < current_size; i++)
 	{
-		cout << array_a[i] << " ";
+		cout << array_1[i] << " ";
 	}
-	
+
 	cout << endl << endl;
-	
-	
+
+	//system("pause");
 	return 0;
 }
+
