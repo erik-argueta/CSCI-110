677eca17cf523a66f67507cd368299af6473d63d
erik-argueta <59490229+erik-argueta@users.noreply.github.com>
2020-02-12 11:27:03 -0800

    touch-ups

--- Labs/Lab006a/Lab006a/Lab006a/main.cpp
+++ Labs/Lab006a/Lab006a/Lab006a/main.cpp
@@ -1,11 +1,12 @@
-//
-//  main.cpp
-//  Ch8-1-2
-//
-//  Created by Erik Argueta on 2/10/20.
-//  Copyright © 2020 Erik Argueta. All rights reserved.
-//
+/*
+Program Description: Runs through text file and prints only names on another file
+Author: Erik Argueta
+Input Variables: in_file, name, count, percent
+Process Flow: scans through the input file and placed only names on the output file
+Output Variables: Output file with only the names of babies and a rank of 153
+*/
 
+#include "stdafx.h"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -13,10 +14,10 @@
 using namespace std;
 
 /**
-	Reads name info, prints the name if total >= 0, and adjusts the total.
- @param in_file the input stream
- @param total the total percentage that should still be processed
- */
+Reads name info, prints the name if total >= 0, and adjusts the total.
+@param in_file the input stream
+@param total the total percentage that should still be processed
+*/
 
 void process_name(ifstream& in_file, ofstream& out_file, float& total)
 {
@@ -25,8 +26,8 @@ void process_name(ifstream& in_file, ofstream& out_file, float& total)
 	int count;
 	float percent;
 	in_file >> name >> count >> percent;
-	
-	if (in_file.fail()) {return;} // Check for failure after each input
+
+	if (in_file.fail()) { return; } // Check for failure after each input
 	if (total > 0) { out_file << name << " "; }
 	total = total - percent;
 }
@@ -34,35 +35,34 @@ void process_name(ifstream& in_file, ofstream& out_file, float& total)
 int main()
 {
 	ifstream in_file;
-	in_file.open("/Users/erik-argueta/Documents/GitHub/CSCI-110/Labs/Lab006a/babynames.txt");
-	if (in_file.fail()) { return 1; } // Check for failure after opening
-	
+	in_file.open("D:\\CSCI110\\Labs\\Lab006a\\babynames.txt"); // "D:\\babynames.txt"
+	if (in_file.fail()) { return 0; } // Check for failure after opening
+
 	ofstream out_file;
-	out_file.open("/Users/erik-argueta/Documents/GitHub/CSCI-110/Labs/Lab006a/OUTPUT.txt");
-	
+	out_file.open("D:\\CSCI110\\Labs\\Lab006a\\OUTPUT.txt"); // "D:\\OUTPUT.txt"
+
 	float boy_total = 50;
 	float girl_total = 50;
-	
-	cout << "Processing \"babynames.txt\"..." << endl << endl;
-	cout << "Processing 153 Lines..." << endl << endl;
-	
+
+	cout << "Processing File..." << endl << endl;
+	cout << "Processing 153 Records..." << endl << endl;
+
 	while (boy_total > 0 || girl_total > 0) // To process each line
 	{
 		int rank;
 		in_file >> rank;
-		if (in_file.fail()) { return 2; }
-		
+		if (in_file.fail()) { return 0; }
+
 		out_file << rank << " ";
-		
-		
-		
+
 		process_name(in_file, out_file, boy_total);
 		process_name(in_file, out_file, girl_total);
-		
+
 		out_file << endl;
 	}
-	
-	cout << "Process Complete." << endl << endl;
-	
+
+	cout << "Process Complete!" << endl << endl;
+
+	system("pause");
 	return 0;
 }
--- Labs/Lab006b/Lab006b/Lab006b/main.cpp
+++ Labs/Lab006b/Lab006b/Lab006b/main.cpp
@@ -1,12 +1,14 @@
-//
-//  main.cpp
-//  Read-Text
-//
-//  Created by Erik Argueta on 2/11/20.
-//  Copyright © 2020 Erik Argueta. All rights reserved.
-//
+/*
+Program Description: Runs through text file and prints a table with the average Author: Erik Argueta
+Input Variables: input,
+Process Flow: scans through input file and calculates the average in the two
+columns
+Output Variables: Output file and terminal with the table and column average
+ */
 
+#include "stdafx.h"
 #include <iostream>
+#include <string>
 #include <fstream>
 #include <iomanip>
 
@@ -25,30 +27,27 @@ float avg(float& total, int size)
 }
 
 int main() {
-	
-	// /Users/erik-argueta/Documents/GitHub/CSCI-110/Labs/Lab006b/numbers.txt
-	
+// D:\\CSCI110\\Labs\\Lab006b\\numbers.txt
+
 	string input;
-	
 	cout << "Please enter the pathway to your file: ";
 	cin >> input;
-	
-	ifstream in_file;
-	in_file.open(input);
+	ifstream in_file; in_file.open(input);
 	if (in_file.fail())
 	{
 		cout << "ERROR: File not found." << endl;
+		system("pause");
 		return 1;
 	}
-	
 	ofstream out_file;
-	out_file.open("//Users/erik-argueta/Documents/GitHub/CSCI-110/Labs/Lab006b/OUTPUT.txt");
+	out_file.open("D:\\CSCI110\\Labs\\Lab006b\\OUTPUT.txt");
 	if (out_file.fail())
 	{
 		cout << "ERROR: File not found." << endl;
+		system("pause");
 		return 2;
 	}
-
+	
 	float num1;
 	float num2;
 	float total1 = 0;
@@ -60,8 +59,17 @@ int main() {
 	int counter3 = 10;
 	int counter4 = 10;
 	int size = 10;
-
+	
 	cout << endl;
+	
+	cout << setw(35) << "============================" << endl;
+	cout << setw(9) << "||" << setw(21) << "LAB6B DATA TABLE" << setw(5) << "||" << endl;
+	cout << setw(35) << "============================" << endl;
+	
+	out_file << setw(35) << "============================" << endl;
+	out_file << setw(9) << "||" << setw(17) << "LAB6B DATA TABLE" << setw(9) << "||" << endl;
+	out_file << setw(35) << "============================" << endl;
+	
 	cout << setw(19) << "----------" << setw(13) << "----------" << endl;
 	cout << setw(19) << "|Column 1|" << setw(13) << "|Column 2|" << endl;
 	cout << setw(19) << "----------" << setw(13) << "----------" << endl;
@@ -70,49 +78,45 @@ int main() {
 	out_file << setw(19) << "|Column 1|" << setw(13) << "|Column 2|" << endl;
 	out_file << setw(19) << "----------" << setw(13) << "----------" << endl;
 	
-	while (counter1 < 10 || counter2 < 10)
+	// Function
 	{
-		in_file >> num1 >> num2;
-		cout << setw(15) << num1 << setw(15) << num2 << endl;
-		out_file << setw(15) << num1 << setw(15) << num2 << endl;
-		
-		
-		
-		counter1++;
-		counter2++;
+		while (counter1 < 10 || counter2 < 10)
+		{
+			in_file >> num1 >> num2;
+			cout << setw(15) << num1 << setw(15) << num2 << endl;
+			out_file << setw(15) << num1 << setw(15) << num2 << endl;
+			counter1++;
+			counter2++;
+			
+		}
+		in_file.close(); in_file.open(input);
+		if (in_file.fail())
+		{
+			cout << "ERROR: File not found." << endl; system("pause");
+			return 3;
+		}
+		while (counter3 > 0 || counter4 > 0)
+		{
+			float num1;
+			float num2;
+			
+			in_file >> num1 >> num2;
+			
+			add(in_file, counter3, total1, num1);
+			add(in_file, counter4, total2, num2);
+			
+		}
+			average1 = avg(total1, size);
+			average2 = avg(total2, size);
 	}
-
-	in_file.close();
-	
-	in_file.open(input);
-	if (in_file.fail())
-	{
-		cout << "ERROR: File not found." << endl;
-		return 3;
-	}
-	
-	while (counter3 > 0 || counter4 > 0)
-	{
-		float num1;
-		float num2;
-		in_file >> num1 >> num2;
-		
-		add(in_file, counter3, total1, num1);
-		add(in_file, counter4, total2, num2);
-	}
-
-	//
-	average1 = avg(total1, size);
-	average2 = avg(total2, size);
 	
 	cout << "-----------------------------------" << endl;
 	out_file << "-----------------------------------" << endl;
-	
-	cout << "Average: " << setw(7) << average1 << setw(15) << average2 << endl;
-	out_file << "Average: " << setw(7) << average1 << setw(15) << average2 << endl;
+	cout << "Average: " << setw(7) << average1 << setw(15) << average2 << endl << endl;
+	out_file << "Average: " << setw(7) << average1 << setw(15) << average2 << endl << endl;
 	
 	in_file.close();
 	out_file.close();
-	
+	system("pause");
 	return 0;
 }
